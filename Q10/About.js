ed921efa366e65b2e020a4f707c4056f849cd764
Shreyash13-bit <22052762@kiit.ac.in>
2025-06-17 06:34:08 +0530

    Create About.js

---
+++ Q10/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from './Layout';
+const About = () => {
+  return (
+    <Layout>
+      <h2>About Page</h2>
+      <p>This page describes the purpose of the project.</p>
+    </Layout>
+  );
+};
+export default About;
