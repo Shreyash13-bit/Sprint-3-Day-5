9cf8578d3400c5c465dd513a1de26b1261a78908
Shreyash13-bit <22052762@kiit.ac.in>
2025-06-17 06:33:40 +0530

    Create Home.js

---
+++ Q10/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from './Layout';
+const Home = () => {
+  return (
+    <Layout>
+      <h2>Home Page</h2>
+      <p>Welcome to the homepage of this simple React layout.</p>
+    </Layout>
+  );
+};
+export default Home;
